218cdaa66144be4fff694a6c64ab394185329bbf
yadhu1904 <73237206+yadhu1904@users.noreply.github.com>
2025-10-16 19:16:37 +0530

    Mask password in MongoDB URI
    Updated MongoDB connection string to mask the password.

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 
-MONGO_URI = "mongodb+srv://yadhuvamship_db_user:EHmOZltZzcdixgZo@mongoyadhu.x3ksszq.mongodb.net"
+MONGO_URI = "mongodb+srv://yadhuvamship_db_user:<password>@mongoyadhu.x3ksszq.mongodb.net"
 
 
 conn = MongoClient(MONGO_URI)
